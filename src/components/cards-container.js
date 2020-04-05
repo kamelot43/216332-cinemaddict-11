2226eeca70fffefe2c6f26309ce9410f6a89d8de
admin <admin@beebops-MacBook-Pro.local>
2020-04-05 18:06:30 +0300

    выполнение задания

---
+++ src/components/cards-container.js
@@ -0,0 +1,15 @@
+export const createCardsContainerTemplate = () => {
+  return (
+    `
+      <section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+
+      <div class="films-list__container">
+
+      </dvi>
+      </section>
+      </section>
+    `
+    )
+};
---
+++ src/components/most-commented-container.js
@@ -0,0 +1,15 @@
+
+export const createMostCommentedContainerTemplate = () => {
+  return (
+    `
+      <section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+
+      <div class="films-list__container">
+
+      </dvi>
+      </section>
+      </section>
+    `
+    )
+};
